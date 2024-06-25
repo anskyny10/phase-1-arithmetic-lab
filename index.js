c03d4bf3274a7172bba940e5512b2f64504f1111
Jason Malefakis <anskyny10@aol.com>
2024-06-25 00:15:45 -0400

    add arithmetic tests

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = (num1 * num2)
+multiply;
+
+let random = Math.ceil(Math.random());
+random;
+
+let num3 = 25
+let num4 = 7
+let mod = num3 % num4
+mod;
+
+let max = Math.max(1, 3, 20, 7)
+max;
